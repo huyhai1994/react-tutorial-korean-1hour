2ba8f533588736b04512033991e6bd822c772464
huyhai1994 <nguyenhuyhai926@gmail.com>
2024-07-15 15:54:53 +0700

    update README.md

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import About from "./pages/About";
 import Counter from "./pages/Counter";
 import Input from "./pages/Input";
 import Input1 from "./pages/Input1";
-
+import List from "./pages/List";
 
 function App() {
     /*TODO: App component -> index.html (div)에 넣어줌 */
@@ -16,13 +16,14 @@ function App() {
         {/*TODO: xml 가 html 모양이 완전하게 100%는 유사하지 않음*/}
         <nav>
             {/*TODO: 나중에 링크는 아에이 태크로 변경됨*/}
-            <Link to="/">Home </Link> |
             {/*TODO: Home 크릭 할때는 Route 를 찾고 해당할 element
                     찾아냄*/}
+            <Link to="/">Home </Link> |
             <Link to="/about">About </Link>|
-            <Link to="/counter">Counter</Link>
-            <Link to="/input">Input</Link>
-            <Link to="/input1">Input1</Link>
+            <Link to="/counter">Counter</Link>|
+            <Link to="/input">Input</Link>|
+            <Link to="/input1">Input1</Link>|
+            <Link to="/list">List</Link>
             <Routes>
                 {/*TODO:화면이 전환해줌*/}
                 <Route path='/' element={<Home/>}/>
@@ -30,6 +31,7 @@ function App() {
                 <Route path='/counter' element={<Counter/>}/>
                 <Route path='/input' element={<Input/>}/>
                 <Route path='/input1' element={<Input1/>}/>
+                <Route path='/list' element={<List/>}/>
             </Routes>
         </nav>
     </div>);
--- src/pages/List.js
+++ src/pages/List.js
@@ -1,31 +1,38 @@
 import React from 'react'
 /*TODO: 서버에서 데이터를 가져 와서
 *       나타내주는 함수   */
-const UserList = () => {
-    const users = [
-        {
-            email: 'user1@example.com',
-            name: 'User 1'
-        },
-        {
-            email: 'user2@example.com',
-            name: 'User 2'
-        },
-        {
-            email: 'user3@example.com',
-            name: 'User 3'
-        },
-        {
-            email: 'user4@example.com',
-            name: 'User 4'
-        }, {
-            email: 'user5@example.com',
-            name: 'User 5'
-        },
-        {
-            email: 'user6@example.com',
-            name: 'User 6'
-        }
-
-    ]
+const User = ({userData}) => {
+    return (<tr>
+        <td>{userData.name}</td>
+        <td>{userData.email}</td>
+    </tr>)
 }
+const UserList = () => {
+    const users = [{
+        email: 'user1@example.com', name: 'User 1'
+    }, {
+        email: 'user2@example.com', name: 'User 2'
+    }, {
+        email: 'user3@example.com', name: 'User 3'
+    }, {
+        email: 'user4@example.com', name: 'User 4'
+    }, {
+        email: 'user5@example.com', name: 'User 5'
+    }, {
+        email: 'user6@example.com', name: 'User 6'
+    }
+
+    ];
+    return (<table>
+        <thead>
+        <tr>
+            <th>Name</th>
+            <th>Email</th>
+        </tr>
+        </thead>
+        <tbody>
+        {users.map(user => <User userData={user}/>)}
+        </tbody>
+    </table>);
+}
+export default UserList;
