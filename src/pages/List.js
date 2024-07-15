c16f037e81dfd35ad2532ad946ff3ff811c5cb8b
huyhai1994 <nguyenhuyhai926@gmail.com>
2024-07-15 15:36:13 +0700

    add List.js (server data)

---
+++ src/pages/List.js
@@ -0,0 +1,31 @@
+import React from 'react'
+/*TODO: 서버에서 데이터를 가져 와서
+*       나타내주는 함수   */
+const UserList = () => {
+    const users = [
+        {
+            email: 'user1@example.com',
+            name: 'User 1'
+        },
+        {
+            email: 'user2@example.com',
+            name: 'User 2'
+        },
+        {
+            email: 'user3@example.com',
+            name: 'User 3'
+        },
+        {
+            email: 'user4@example.com',
+            name: 'User 4'
+        }, {
+            email: 'user5@example.com',
+            name: 'User 5'
+        },
+        {
+            email: 'user6@example.com',
+            name: 'User 6'
+        }
+
+    ]
+}
